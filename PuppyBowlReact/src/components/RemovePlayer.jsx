6150e4048b92adb133bcf3c19927ee7ac2678d85
John Fuller <fuller.john84@gmail.com>
2024-01-23 19:02:53 -0500

    Fixed code for add/remove players

--- PuppyBowlReact/src/components/RemovePlayer.jsx
+++ PuppyBowlReact/src/components/RemovePlayer.jsx
@@ -1,24 +1,5 @@
 import { API_URL } from "../API";
 
-const RemovePlayer = async (playerId) => {
-  try {
-    const response = await fetch(`${API_URL}/${playerId}`, {
-      method: "DELETE",
-    });
-
-    if (!response.ok) {
-      throw new Error("Fetch failed to remove player.");
-    }
-
-    const result = puppies.filter((player) => player.id !== playerId);
-    setPuppies(result);
-    console.log(result);
-  } catch (err) {
-    console.error(
-      `Whoops, trouble removing player #${playerId} from the roster!`,
-      err
-    );
-  }
-};
+const RemovePlayer = async (playerId) => {};
 
 export default RemovePlayer;
